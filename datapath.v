3eef9edefdbe2145f40f425e24533a62c3536099
sussa1 <assuncao26vitor@gmail.com>
2020-09-26 22:43:27 -0300

    ORI ok

--- datapath.v
+++ datapath.v
@@ -14,7 +14,7 @@ module fetch (input takebranch, rst, clk, branch, input [31:0] sigext, output [3
   initial begin
     // Exemplos
     inst_mem[0] <= 32'h00000000; // nop
-    inst_mem[1] <= 32'b00000000000100110110010000010011; // ori x8, x6, 0
+    inst_mem[1] <= 32'b00000000000100110110010000010011; // ori x8, x6, 1 ok
     inst_mem[2] <= 32'b00000000010000011001000110010011; // slli x3, x3, 4 ok
     inst_mem[3] <= 32'b00000000101001100000000110000111; // lwi x3, x6, x10 ok
     //inst_mem[3] <= 32'b11111110000100001000110011100011; // beq x1, x1, -8  ok
